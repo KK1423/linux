360957ac2e572c7723422830aef54be39aa8c4e7
Karthik Karyamapudi <karthikkaryamapudi@gmail.com>
2017-11-26 22:20:13 -0500

    Modded poweroff module and added dts file for cardac project

--- drivers/power/reset/gpio-poweroff.c
+++ drivers/power/reset/gpio-poweroff.c
@@ -30,13 +30,6 @@ static void gpio_poweroff_do_poweroff(void)
 	BUG_ON(!reset_gpio);
 
 	/* drive it active, also inactive->active edge */
-	gpiod_direction_output(reset_gpio, 1);
-	mdelay(100);
-	/* drive inactive, also active->inactive edge */
-	gpiod_set_value(reset_gpio, 0);
-	mdelay(100);
-
-	/* drive it active, also inactive->active edge */
 	gpiod_set_value(reset_gpio, 1);
 
 	/* give it some time */
